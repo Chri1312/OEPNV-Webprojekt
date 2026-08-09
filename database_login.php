73e288faba6fb608c02b526a777dff9b83d3c5e9
Chri1312 <148619028+Chri1312@users.noreply.github.com>
2026-08-09 17:43:18 +0200

    Add files via upload

---
+++ database_login.php
@@ -0,0 +1,6 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "oepnv_stationen_berlin";
+?>
---
+++ eingabe_verarbeiten.js
@@ -0,0 +1,90 @@
+document.addEventListener("DOMContentLoaded", function() {
+get_stations();
+
+const input_field = document.getElementById("station_input");
+const out = document.getElementById("out");
+const next = document.getElementById("next_round");
+const suggest_div = document.getElementById("suggest");
+
+input_field.addEventListener("input", suggest);
+suggest_div.addEventListener("mousedown", use_input)
+next.addEventListener("mousedown", reset_html)
+
+let html_content = "";
+async function use_input(event) {
+    let station = event.target.innerHTML;
+    input_field.value = "";
+    suggest_div.innerHTML = "";
+    try {
+        const resp = await fetch("auswerten.php", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({Station: station})
+        });
+
+        if (!resp.ok) {
+            throw new Error(`Netzwerkfehler: ${resp.status}`);
+        }
+
+        const data = await resp.json();
+        if (data.Error) {
+            html_content += "<p> FEHLER! </p>"
+        } else {
+            if (data.direction != "") {
+                html_content += "<div id=guess><p> Die gesuchte Station liegt im <b>" + data.direction + "</b> von <i><b>" + data.guess + "</b></i> </p>";
+                html_content += "<p id=" + data.correct_lines + ">" + data.lines + "</p></div>"
+            } else {
+                html_content += "<div id=guess><p> Die gesuchte Station wurde gefunden: <i><b>" + data.guess + "</b></i> </p>";
+                html_content += "<p id=" + data.correct_lines + ">" + data.lines + "</p></div>";
+                next.innerHTML = "<p id=suggestion>Nächste Runde</p>";
+                input_field.setAttribute("disabled", "disabled");
+            }
+        }
+    } catch(e) {
+        html_content = "<p>" + e + "</p>";
+    }
+
+    out.innerHTML = html_content;
+}
+
+let stationsliste = [];
+async function get_stations() {
+    try {
+        const resp = await fetch("data_req.php", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({Station: "All"})
+        });
+
+        if (!resp.ok) {
+            throw new Error(`Netzwerkfehler: ${resp.status}`);
+        }
+
+        const data = await resp.json();
+        stationsliste = data;
+
+    } catch (e) {}
+}
+
+let html_suggest = "";
+function suggest(event) {
+    html_suggest = "";
+
+    let limit = 3;
+    for (let i = 0; i < stationsliste.length; i++) {
+        if (stationsliste[i][0].toLowerCase().startsWith(event.target.value.toLowerCase()) && limit > 0 && event.target.value != "") {
+            html_suggest += "<p>" + stationsliste[i][0] + "</p>";
+            limit -= 1;
+        }
+    }
+    suggest_div.innerHTML = html_suggest;
+}
+
+function reset_html() {
+    html_content = "";
+    out.innerHTML = "";
+    suggest_div.innerHTML = "";
+    next.innerHTML = "";
+    input_field.removeAttribute("disabled");
+}
+});
